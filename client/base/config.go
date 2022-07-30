29fe1213f673182b9b78a91d9397e28e88a4fa4a
vvictorl <102740406+vvictorl@users.noreply.github.com>
2022-07-31 01:35:44 +0900

    feat: make encoding config setter

---
+++ client/base/config.go
@@ -0,0 +1,51 @@
+package base
+
+import (
+	"github.com/Carina-hackatom/coordinator/client/base/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/std"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+)
+
+type EncodingConfig struct {
+	InterfaceRegistry codectypes.InterfaceRegistry
+	Marshaler         codec.Codec
+	TxConfig          client.TxConfig
+	Amino             *codec.LegacyAmino
+}
+
+func makeEncodingConfig() EncodingConfig {
+	amino := codec.NewLegacyAmino()
+	ir := codectypes.NewInterfaceRegistry()
+	marshaler := codec.NewProtoCodec(ir)
+	txCfg := authtx.NewTxConfig(marshaler, authtx.DefaultSignModes)
+
+	return EncodingConfig{
+		InterfaceRegistry: ir,
+		Marshaler:         marshaler,
+		TxConfig:          txCfg,
+		Amino:             amino,
+	}
+}
+
+func MakeEncodingConfig(mb module.BasicManager) EncodingConfig {
+	encCfg := makeEncodingConfig()
+	std.RegisterLegacyAminoCodec(encCfg.Amino)
+	std.RegisterInterfaces(encCfg.InterfaceRegistry)
+	mb.RegisterLegacyAminoCodec(encCfg.Amino)
+	mb.RegisterInterfaces(encCfg.InterfaceRegistry)
+
+	return encCfg
+}
+
+func SetBechPrefix() {
+	config := sdktypes.GetConfig()
+	config.SetBech32PrefixForAccount(types.Bech32PrefixAccAddr, types.Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(types.Bech32PrefixValAddr, types.Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(types.Bech32PrefixConsAddr, types.Bech32PrefixConsPub)
+	config.Seal()
+}
